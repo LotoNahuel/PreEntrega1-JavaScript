ef4b418b58141889057bf4c6fbfc40dcd59b80d2
NAHUEL LOTO <loto_nahuel@hotmail.com>
2023-11-03 16:49:00 -0300

    correccionDeIfAndWhile

--- js/script.js
+++ js/script.js
@@ -27,8 +27,8 @@ if(iniciar == "si"){
     }
     tipoMoneda()
     
-    if(end_or_not === "si"){
-        while(tipoPrecio === "dolar"){
+    if(end_or_not == "si"){
+        while(tipoPrecio == "dolar"){
             precio = parseFloat(prompt("Ingrese un precio:"))
             end_or_not = prompt("Quiere ingresar otro precio:")
             tipoMoneda()
@@ -36,7 +36,7 @@ if(iniciar == "si"){
                 break
             }
         }
-        while(tipoPrecio === "peso"){
+        while(tipoPrecio == "peso"){
             precio = parseFloat(prompt("Ingrese un precio:"))
             end_or_not = prompt("Quiere ingresar otro precio:")
             tipoMoneda()
